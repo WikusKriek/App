5419f5794dffe8f34aac3971631cd7e16efd947a
wikuskriek <wikus.kriek.2w@gmail.com>
2023-10-13 07:28:27 +0200

    feat: textLink in header

--- src/components/ParentNavigationSubtitle.js
+++ src/components/ParentNavigationSubtitle.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styles from '../styles/styles';
-import CONST from '../CONST';
 import Text from './Text';
-import PressableWithoutFeedback from './Pressable/PressableWithoutFeedback';
 import Navigation from '../libs/Navigation/Navigation';
 import ROUTES from '../ROUTES';
 import useLocalize from '../hooks/useLocalize';
+import TextLink from './TextLink';
 
 const propTypes = {
     parentNavigationSubtitleData: PropTypes.shape({
@@ -19,15 +18,10 @@ const propTypes = {
 
     /** parent Report ID */
     parentReportID: PropTypes.string,
-
-    /** PressableWithoutFeedack additional styles */
-    // eslint-disable-next-line react/forbid-prop-types
-    pressableStyles: PropTypes.arrayOf(PropTypes.object),
 };
 
 const defaultProps = {
     parentReportID: '',
-    pressableStyles: [],
 };
 
 function ParentNavigationSubtitle(props) {
@@ -36,23 +30,29 @@ function ParentNavigationSubtitle(props) {
     const {translate} = useLocalize();
 
     return (
-        <PressableWithoutFeedback
-            onPress={() => {
-                Navigation.navigate(ROUTES.REPORT_WITH_ID.getRoute(props.parentReportID));
-            }}
-            accessibilityLabel={translate('threads.parentNavigationSummary', {rootReportName, workspaceName})}
-            accessibilityRole={CONST.ACCESSIBILITY_ROLE.LINK}
-            style={[...props.pressableStyles]}
+        <Text
+            style={[styles.optionAlternateText]}
+            numberOfLines={1}
+            textBreakStrategy="highQuality"
         >
-            <Text
-                style={[styles.optionAlternateText]}
-                numberOfLines={1}
+            <Text style={[styles.optionAlternateText, styles.textLabelSupporting]}>{`${translate('threads.from')} `}</Text>
+            <TextLink
+                onPress={() => {
+                    Navigation.navigate(ROUTES.REPORT_WITH_ID.getRoute(props.parentReportID));
+                }}
+                style={[styles.optionAlternateText, styles.textLabelSupporting, styles.link]}
             >
-                <Text style={[styles.optionAlternateText, styles.textLabelSupporting]}>{`${translate('threads.from')} `}</Text>
-                <Text style={[styles.optionAlternateText, styles.textLabelSupporting, styles.link]}>{rootReportName}</Text>
-                {Boolean(workspaceName) && <Text style={[styles.optionAlternateText, styles.textLabelSupporting]}>{` ${translate('threads.in')} ${workspaceName}`}</Text>}
-            </Text>
-        </PressableWithoutFeedback>
+                {rootReportName}
+            </TextLink>
+            {Boolean(workspaceName) && (
+                <TextLink
+                    onPress={() => {
+                        Navigation.navigate(ROUTES.REPORT_WITH_ID.getRoute(props.parentReportID));
+                    }}
+                    style={[styles.optionAlternateText, styles.textLabelSupporting]}
+                >{` ${translate('threads.in')} ${workspaceName}`}</TextLink>
+            )}
+        </Text>
     );
 }
 
